7ff2539ecf4e3c3396ef85e915c66a1256f79146
lgchew16 <53231978+lgchew16@users.noreply.github.com>
2019-09-10 12:16:29 -0700

    Add files via upload

--- SpringRec.py
+++ SpringRec.py
@@ -6,7 +6,7 @@ Created on Sun Jul 28 15:38:45 2019
 """
 
 import pandas as pd
-import numpy as np
+#import numpy as np
 #import matplotlib.pyplot as plt
 
 # load CSV data
@@ -15,5 +15,14 @@ filenamein='FOL_in'
 
 ############### SPRING RECESSION ##################
 #df.resample('AS-OCT')
-	
+'''Create Spring DataFrame'''
+ix=(df.index.month>3) & ((df.index.month<=6) & (df.index.day<=15)) #arbitrary dates hardcoded
+spring_df=df[ix]
 
+'''Duration'''
+
+
+'''Rate of Change'''
+
+ix=(spring_df.diff()<0)
+RateOfChange=spring_df[ix].resample('AS-OCT').median()
