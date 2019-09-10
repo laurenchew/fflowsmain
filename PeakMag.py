725fa3c81141d577d2424ca27a93fd9b5b3d745c
lgchew16 <53231978+lgchew16@users.noreply.github.com>
2019-09-10 12:04:18 -0700

    Add files via upload

--- PeakMag.py
+++ PeakMag.py
@@ -25,44 +25,55 @@ def file_name_dur(p):
 	filenameout=filenamein+'_PM'+pct+'%Dur'+'.csv'
 	return filenameout
 def file_name_freq(p):
-	pct=' %.f'%((1-p)*100) =
+	pct=' %.f'%((1-p)*100) 
 	filenameout=filenamein+'_PM'+pct+'%Freq'+'.csv'
 	return filenameout
 
 '''Magnitude''' #Median value of flow magnitudes above thld
-def peak_mag_calc(df_local):
+def magnitude_calc(df_local):
 	thld=df_local.quantile(q=p) #exceedance values
 	ix=(df_local>thld) 
 	pm_df=df_local[ix]
 	return pm_df.resample('AS-OCT').median() 
 
 '''Duration''' #Number of days in each year above the threshold
-def dur(df_local):
+def duration_calc(df_local):
 	thld=df_local.quantile(q=p)	
 	ix=(df_local>thld)*1	
-	dur=ix.resample('AS-OCT').sum() #Wants median number of days that a flow stays over the threshold, not just how many days in a year
-	return dur
+	#dur=ix.resample('AS-OCT').sum() #Wants median number of days that a flow stays over the threshold, not just how many days in a year
+	#dur=(ix.diff==1)
+	dur=pd.DataFrame() #creates empty df
+	print (ix.diff)
+	if ix.diff==1:
+		count=1
+		something=0
+		dur=pd.DataFrame() #creates empty df
+		while ix.diff==0:
+			count=count+1
+			something=something+ix
+		dur=dur.append(something)
+	#want year of index preserved if using resample in function
+	#want just an array with counts if annually outside when .apply
+	average_dur=dur.median()	
+	#average_dur=dur.resample('AS-OCT').median()
+	return average_dur
 
 '''Frequency''' #Number of times a flow crosses exceedance flow threshold
-def freq(df_local):
+def frequency_calc(df_local):
 	thld=df_local.quantile(q=p)	
 	ix=(df_local>thld)*1	
-#	count=0
-#	for i in range(1,len(ix)): 
-#		if ix[i]>ix[i-1]:
-#			count=count+1
-	count=(ix.diff()==1).sum()
-		#print(i)
-	#count=count/2
-	return count
+	freq=(ix.diff()==1).resample('AS-OCT').sum() #options are -1,1 or 0
+	return freq
 
 percentile=[.98,.95,.90,.80]
 for p in percentile:
 	#thld=df.quantile(q=p,numeric_only=True) #had issue with non-identically-labeled Series objects if used thld out of function
-	peak_mag=df.apply(peak_mag_calc) #it already knows 1 col at a time
+	peak_mag=df.apply(magnitude_calc) #it already knows 1 col at a time
 	#peak_mag.to_csv(file_name_mag(p))
-	duration=df.apply(dur)
+	#duration=df.apply(duration_calc)
+	#duration=df.resample('AS-OCT').apply(duration_calc)
+	duration=df.iloc[0:365,0].apply(duration_calc)
 	#duration.to_csv(file_name_dur(p))
-	frequency=df.resample('AS-OCT').apply(freq)
+	frequency=df.apply(frequency_calc)
 	#frequency.to_csv(file_name_freq(p))
 	#Even in 20th percentile there are years that do not have a PM, calculate thld differntly?
