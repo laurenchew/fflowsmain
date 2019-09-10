6e4c45cd6b5cb1040e25eb43c237793792ccf255
lgchew16 <53231978+lgchew16@users.noreply.github.com>
2019-09-10 12:17:55 -0700

    Add files via upload

--- WetSeason.py
+++ WetSeason.py
@@ -26,21 +26,29 @@ wetstartdf=df[ix] #or df.loc[ix] does same
 #if baseflow[df.index.year=baseflow[df.index.year-1]]>3:
 #	baseflow=previous year
 #else baseflow=3
+dummyibf=2*df.resample('AS-OCT').min()
 
-#for i in range(1,numyr):
-#	yr=min(df.index.year)+i
-#	year[0,i]=df[df.index.year == yr].to_csv
-#	bf= baseflow[baseflow.index.year==min(df.index.year)+i-1]
-#	drystart= index
-#	bool= bf<3 #Gives matrix of true and false
+def baseflow_calc(df_resampled_local):
+	for i in range(len(dummyibf)):
+		ix= (dummyibf[i]<df_resampled_local)
+		bf=df_resampled_local[ix]
+		#is there a way to replace only some of the values? merge two, like multiply true values by 3 and fill zeros with corresponding
+#	if bf<3 #Gives matrix of true and false
+#		bf=3
 #	#bf[bool==False]=3 #more complicated
 #	if bf.value<3:
 #		bf.value=3
 #wetstart= index where magnitude>2*bf,need to run through from OCT 1 to SEP 31
-#idry=((df.index.month>5)& (df.index.day<16)) & (df.index.month<10) 
-#potentialdry=df[idry]
-#iwet=(df.index.month>9) & ((df.index.month<13) & (df.index.day<16))
-#potentialdry=df[iwet]
+baseflow=df.resample('AS-OCT').apply(baseflow_calc)
+	
+'''Baseflow Wet Season'''
+wetstartm=10 #will put in true values later
+wetstartd=1
+wetendm= 3
+wetendd=15
+ix = ((df.index.month >= wetstartm)&(df.index.day>=wetstartd)) & ((df.index.month <=wetendm)&(df.index.day<=wetendm)) # only summer
+wet_df=df[ix]
+baseflow=df.quantile(q=0.1)
 
 
 #peak=df[ix].resample('AS-OCT').max()
